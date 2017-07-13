3fbfa4986d52255a630a008f335252cbe86079f9
Collin <pineapplepie4@yahoo.com>
2017-07-12 21:16:39 -0700

    add real questions and divs for html

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,2 @@
 $(document).ready(function() {
    $('input[type="radio"]').click(function() {
-       if($(this).attr('id') == 'optionsRadios17') {
-            $('#radio').show();
-       }
-
-       else {
-            $('.rq2').show();
-       }
-   });
-  $(".btn").click(function()  {
-    $(".text").hide();
-    $("#language").show();
-  });
-});
