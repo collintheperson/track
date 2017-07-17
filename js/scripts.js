11207c678a9605a54606af62a6a6446420cff1b3
Collin <pineapplepie4@yahoo.com>
2017-07-16 18:12:48 -0700

    add branching for first radio button

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
 $(function() {
    $("#formID").submit(function(event) {
-     alert("hello");
-     var = "php1"  //So set a variable equal to the value of one. $().val(). So i have a variable tbis variable equals this string do this statement.
-     if ()
-     ($('input[name=radio1]:checked')> 0 ) {
-      //  $("#php").show();
-      alert("yo")
+     var answer = $("input:radio[name=radio1]:checked").val();
+      //So set a variable equal to the value of one. $().val(). So i have a variable tbis variable equals this string do this statement.
+      alert(answer)
+     if (answer === "php1"){
+      //  alert("this work")
+       $("#php").show();
      }
+    //    $("#php").show();
+    //   alert(answer)
+    //  }
 
 
       event.preventDefault();
